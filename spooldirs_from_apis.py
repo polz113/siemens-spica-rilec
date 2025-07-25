0d07d93992680c5ac53f921ba3f3a7d7a2a92b8e
polz <polz@fri.uni-lj.si>
2025-07-25 23:05:13 +0200

    Popravljen spooldirs_from_apis - deluje tudi z UPN

--- spooldirs_from_apis.py
+++ spooldirs_from_apis.py
@@ -4,21 +4,22 @@ import argparse
 import csv
 import os
 
+import urllib.parse
 import urllib.request
 import json
 
 from siemens_spica_settings import SPOOL_DIR, ULID_SPOOL_DIR, FIX_FNAME, NOCOMMIT_FNAME
-from apis_preslikava_kadrovskih_settings import APIS_USERS_URL, APIS_API_KEY
+from apis_preslikava_kadrovskih_settings import APIS_USERS_URL, APIS_API_KEY, APIS_WEB_SPOOL_FIELD, APIS_MAIN_SPOOL_FIELD
 
-FIX_OWNER=1001
-FIX_GROUP=33
+#FIX_OWNER=1001
+#FIX_GROUP=33
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Prenesi podatke na spico')
+    parser = argparse.ArgumentParser(description='Ustvari imenike za spool na osnovi apis')
     parser.add_argument('--spooldir', dest='spooldir', action='store',
                     default=SPOOL_DIR,
                     help='Glavni imenik s podatki')
-    parser.add_argument('--ulidspooldir', dest='ulidspooldir', action='store',
+    parser.add_argument('--webspooldir', dest='webspooldir', action='store',
                     default=ULID_SPOOL_DIR, type=str,
                     help='Imenik za web server')
     parser.add_argument('--apikey', dest='apikey', action='store',
@@ -28,93 +29,89 @@ if __name__ == '__main__':
                     default=APIS_USERS_URL, type=str,
                     help='URL')
     args = parser.parse_args()
-    ulidspooldir = args.ulidspooldir
+    webspooldir = args.webspooldir
     spooldir = args.spooldir
-    url = args.url
+    params = urllib.parse.urlencode([("format", "json"), 
+                                     ("fieldname", APIS_MAIN_SPOOL_FIELD),
+                                     ("fieldname", APIS_WEB_SPOOL_FIELD) ])
     apikey = args.apikey
     headers = {
         "Authorization": "Api-Key " + apikey}
-    req = urllib.request.Request(url, data=None, headers=headers)
-    uids = dict()
+    req = urllib.request.Request(args.url + "?%s" % params, data=None, headers=headers)
+    data = dict()
     try:
         response = urllib.request.urlopen(req)
         for obj in json.loads(response.read()):
-            kadrovska = obj["value"]
             uid = obj["uid"]
-            s = uids.get(uid, set())
-            s.add(kadrovska)
-            uids[obj["uid"]] = s
+            field = obj['field']
+            dd = data.get(field, dict())
+            ds = dd.get(uid, set())
+            ds.add(obj['value'])
+            dd[uid] = ds
+            data[field] = dd
     except urllib.request.HTTPError as error:
         print("The request failed with status code: " + str(error.code))
         print(error.info())
         print(json.loads(error.read()))
     trashdir = os.path.join(spooldir, "conflicts")
-    for ulid, kadrovske in uids.items():
-        ulid_f = os.path.join(ulidspooldir, ulid)
-        others = set()
-        kadrovske_l = list(sorted(kadrovske))
-        dests = []
-        for kadrovska in kadrovske_l:
-            kadrovska_f = os.path.join(spooldir, kadrovska)
-            dests.append(kadrovska_f)
-        dests.append(ulid_f)
-        # print(f"dests: {dests}")
-        kadrovska_fdir = None
-        if not(os.path.islink(dests[0])) and os.path.isdir(dests[0]):
-            kadrovska_fdir = dests[0]
-        for dest in dests:
-            while os.path.islink(dest):
-                others.add(dest)
-                dest = os.path.realpath(os.readlink(dest))
-            others.add(dest)
-        olddir = None
-        others.discard(kadrovska_fdir)
-        already_ok = set()
-        # print(f" others:{others}")
-        for i in others:
-            if kadrovska_fdir is None:
-                if not os.path.islink(i) and os.path.isdir(i):
-                    olddir = i
-                    # print(f"olddir: {i}")
-                    continue
-            trash_fname = os.path.join(trashdir, os.path.basename(i))
-            if os.path.exists(i):
-                rl = None
-                islink = os.path.islink(i)
-                if islink:
-                    rl = os.readlink(i)
-                if kadrovska_fdir is not None and islink and rl == kadrovska_fdir:
-                    already_ok.add(i)
-                else:
-                    # print(f" fdir: {kadrovska_fdir}, {i} -> {rl} islink: {islink}")
-                    #if os.path.exists(trash_fname):
-                    #    os.path.unlink(trash_fname)
-                    print(f"mv {i} {trash_fname}")
-                    os.rename(i, trash_fname)
-        if kadrovska_fdir is None:
-            kadrovska_fdir = os.path.join(spooldir, kadrovske_l[0])
-            if olddir is None:
-                print(f"mkdir {kadrovska_fdir}")
-                os.mkdir(kadrovska_fdir)
-            else:
-                print(f"mv {olddir} {kadrovska_fdir}")
-                os.rename(olddir, kadrovska_fdir)
-        already_ok.add(kadrovska_fdir)
-        for dest in dests:
-            if dest not in already_ok:
-                print(f"ln {kadrovska_fdir} {dest}")
-                os.symlink(kadrovska_fdir, dest)
-                already_ok.add(dest)
-        # print("kadrovska_fdir:", kadrovska_fdir, nove_kadrovske + [ulid_f])
-        fix_f = os.path.join(ulid_f, FIX_FNAME)
+    # determine main spool dirs
+    spool_dirs = dict()
+    main_by_ulid = dict()
+    for ulid, vals in data[APIS_MAIN_SPOOL_FIELD].items():
+        l = [ os.path.join(spooldir, i) for i in sorted(vals) ]
+        if l[0] in spool_dirs:
+            print("ERROR: same spool for multiple ULIDs")
+            print("    dirs: ", spool_dirs[l[0]], l)
+            print("    ulid: ", ulid)
+            continue
+        main_by_ulid[ulid] = l[0]
+        spool_dirs[l[0]] = l[1:]
+    # create / rename main spool dirs
+    for main, others in spool_dirs.items():
+        old_dirs = []
+        for o in others:
+            if not os.path.islink(o) and os.path.isdir(o):
+                old_dirs.append(o)
+        if len(old_dirs) > 0 and (os.path.islink(main) or not os.path.isdir(main)):
+            if os.path.exists(main):
+                trash_fname = os.path.join(trashdir, os.path.basename(main))
+                print("mv1 ", main, trash_fname)
+                os.rename(main, trash_fname)
+            print("mv2 ", old_dirs[0], main)
+            os.rename(old_dirs[0], main)
+        if not os.path.exists(main):
+            os.mkdir(main)
+    # create symlinks in the general spool dir
+    for main, others in spool_dirs.items():
+        for o in others:
+            if os.path.islink(o) and os.readlink(o) == main:
+                continue
+            if os.path.exists(o):
+                trash_fname = os.path.join(trashdir, os.path.basename(o))
+                print("mv3 ", o, trash_fname)
+                os.rename(o, trash_fname)
+            os.symlink(main, o)
+    # create symlinks in the web spool dir
+    for ulid, vals in data[APIS_WEB_SPOOL_FIELD].items():
+        if ulid not in main_by_ulid:
+            print("ERROR: missing main spool dir for ulid ", ulid)
+            continue
+        l = [ os.path.join(webspooldir, i) for i in sorted(vals) ]
+        for path in l:
+            if (os.path.islink(path)):
+                os.unlink(path)
+            os.symlink(main_by_ulid[ulid], path)
+    # create missing files / fix permissions
+    for ulid, main in main_by_ulid.items():
+        fix_f = os.path.join(main, FIX_FNAME)
         if not os.path.exists(fix_f):
             with open(fix_f, "a"):
                 pass
-        os.chown(fix_f, FIX_OWNER, FIX_GROUP)
+        # os.chown(fix_f, FIX_OWNER, FIX_GROUP)
         os.chmod(fix_f, 0o660,)
-        nocommit_f = os.path.join(ulid_f, NOCOMMIT_FNAME)
+        nocommit_f = os.path.join(main, NOCOMMIT_FNAME)
         if not os.path.exists(nocommit_f):
             with open(nocommit_f, "a"):
                 pass
-        os.chown(nocommit_f, FIX_OWNER, FIX_GROUP)
+        # os.chown(nocommit_f, FIX_OWNER, FIX_GROUP)
         os.chmod(nocommit_f, 0o660,)
